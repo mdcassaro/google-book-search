9e797d1e4c43e5db7d8d04b47f362d7c8044e665
Michael Cassaro <mdcassaro@hotmail.com>
2020-05-20 10:48:05 -0500

    Updates

---
+++ precache-manifest.fc18ff6c74899e4e960fe822dfe7a71e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c34f18529fe71e6044c802204bbe0d9",
+    "url": "/google-book-search/index.html"
+  },
+  {
+    "revision": "88466a9e0a716d3e13ad",
+    "url": "/google-book-search/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "caf6df3d9a8980888f72",
+    "url": "/google-book-search/static/js/2.72eccf12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/google-book-search/static/js/2.72eccf12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88466a9e0a716d3e13ad",
+    "url": "/google-book-search/static/js/main.aa7a4fcc.chunk.js"
+  },
+  {
+    "revision": "3f33538c087b42dd6692",
+    "url": "/google-book-search/static/js/runtime-main.88ec0799.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-book-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
